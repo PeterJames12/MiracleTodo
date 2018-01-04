dbb2c738af1c9febe1cfd60d2e6c69317b78e7ab
Андрей <i.popolin@gmail.com>
2018-01-04 22:00:21 +0200

    LoginPage

--- src/main/java/controller/LoginPageController.java
+++ src/main/java/controller/LoginPageController.java
@@ -1,27 +1,57 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import service.UserService;
+import service.impl.UserServiceImpl;
+import start.AppManager;
+import util.NotificationUtil;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginPageController implements Initializable {
 
     @FXML
-    private Button LoginButton;
+    private Button loginButton;
     @FXML
-    private TextField EmailTextField;
+    private TextField emailTextField;
     @FXML
-    private PasswordField PassPasswordField;
+    private PasswordField passPasswordField;
     @FXML
-    private Button RegisterButton;
+    private Button registerButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loginButton.setOnAction(event -> checkLogAndPass());
+        registerButton.setOnAction(event -> newRegister());
+    }
 
+    private void checkLogAndPass() {
+        UserService userService = new UserServiceImpl();
+        boolean checkPass = userService.checkPass(emailTextField.getText(), passPasswordField.getText());
+        if (checkPass) {
+            try {
+                AppManager.getStage().setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainPage.fxml"))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            NotificationUtil.notification("Error", "Could not enter", "Try again");
+        }
+    }
+
+    private void newRegister() {
+        try {
+            AppManager.getStage().setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/RegisterPage.fxml"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/start/AppManager.java
+++ src/main/java/start/AppManager.java
@@ -7,24 +7,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.Getter;
 import model.User;
 import util.NotificationUtil;
 
 public class AppManager extends Application{
 
-    public static void main(String[] args) {
+    @Getter
+    private static Stage stage;
+
+   /* public static void main(String[] args) {
         UserDao userDao = new UserDaoImpl();
         final User byEmail = userDao.getByEmail("joyukr@ukr.net");
         System.out.println(byEmail);
-    }
+    }*/
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(""));
+        stage = primaryStage;
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"));
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("");
+//        scene.getStylesheets().add("");
         primaryStage.setScene(scene);
         primaryStage.setTitle("MiracleIT");
-//        primaryStage.show();
+        primaryStage.show();
     }
 }
